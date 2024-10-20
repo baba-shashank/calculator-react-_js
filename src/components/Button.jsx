76ca67a911fa7e53c32d896ca9ee315757ad4ce1
baba-shashank <shashanknayak0702@gmail.com>
2024-10-20 20:28:55 +0530

    this is a final commit

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,24 @@
 import styles from "./Button.module.css";
 function Button() {
-  let arr = ["c"];
+  let arr = [
+    "c",
+    "1",
+    "2",
+    "+",
+    "3",
+    "4",
+    "-",
+    "5",
+    "6",
+    "*",
+    "7",
+    "8",
+    "/",
+    "=",
+    "9",
+    "0",
+    ".",
+  ];
   return (
     <div className={styles.Buton}>
       {arr.map((item) => (
